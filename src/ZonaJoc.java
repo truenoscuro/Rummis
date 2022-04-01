7e7b60cc3b67fe50cf8cc645af496f9907693998
Nofre Ruiz <nofreruizsalom@gmail.com>
2022-04-01 16:32:33 +0200

    Creacio de funcions ZonaJoc

--- src/ZonaJoc.java
+++ src/ZonaJoc.java
@@ -1,4 +1,27 @@
-public class ZonaJoc {
+import java.util.ArrayList;
 
-    
+public class ZonaJoc {
+    ArrayList<Ma> grupCarta;
+
+    public ZonaJoc(){ grupCarta = new ArrayList<>();}
+
+
+    public Carta seleccionar(int g , int c){ return grupCarta.get(g).seleccionar(c); }
+    public void extreure(int g , Carta carta){ grupCarta.get(g).robar(carta); }
+
+
+
+    public void agregarMa(Ma grup){ grupCarta.add(grup); }
+    public void agregarCarta(int g, Carta carta){ grupCarta.get(g).robar(carta); }
+
+
+
+    public void imprimir(){
+        for(int i = 0; i < grupCarta.size();i++){
+            System.out.print(i+": ");
+            grupCarta.get(i).imprimir();
+        }
+    }
+
+
 }
