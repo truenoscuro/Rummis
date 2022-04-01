1e19e4b593f7403fce17c13d8c5b1c296e87cc87
Nofre Ruiz <nofreruizsalom@gmail.com>
2022-04-01 16:14:15 +0200

    Eliminació del cementeri

--- src/Taula.java
+++ src/Taula.java
@@ -4,11 +4,8 @@ public class Taula {
 
     public Taula( int numJugadors ){ jugadors = new Jugador[numJugadors]; }
 
-    public static void CementeriAMazo(Mazo mazo, Cementeri cementeri){ // Canviar aquest nom
-        Carta primeraCementeri = cementeri.extreure();
-        while(!cementeri.esBuid()) mazo.agregar( cementeri.extreure() );
-        mazo.barallar();
-        cementeri.agregar(primeraCementeri);
-    }
+
+
+
 
 }
